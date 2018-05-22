71b9440257241f0aa343ed0eaf5bb25e3d49939e
Jiahong Rong <jiahongrong@Jiahongs-MacBook-Pro.local>
2018-05-21 19:39:53 -0500

    add favorites button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function(){
 
-    $("#weathBtn").on("click", function(){
+    $("#likeBtn").hide();
+    $("#submitBtn").on("click", function(){
+        $("#likeBtn").show();
         $("#weatherDisplay").empty();
             var lattitude = $("#latSearch").val().trim();
             var longtitude = $("#longSearch").val().trim();
@@ -16,50 +18,45 @@ $(document).ready(function(){
             then(function(response){
                     // get the page IDs        
 
-            var weatherInfo=$("<table>");
-                weatherInfo.addClass("card mt-3 p-3");
-            var name=$("<h4>");
-                name.text("The Country is: " + response.city.name);
+            var weatherInfo=$("<table>").addClass("card mt-3 p-3");
+            var name=$("<h4>").text("The Country is: " + response.city.name);
                 console.log("The Country is: " + response.city.name);
             weatherInfo.append(name);       
             for (var i = 0; i <=32; i+=8) {
-                var date=$("<p>");
-                    date.text("Date: " + response.list[i].dt_txt);
+                var date=$("<p>").text("Date: " + response.list[i].dt_txt);
                     console.log("Date: " + response.list[i].dt_txt);  
-                var weather=$("<p>");
-                    weather.text("Local Weather is: " + response.list[i].weather[0].description);
+                var weather=$("<p>").text("Local Weather is: " + response.list[i].weather[0].description);
                     console.log("Local Weather is: " + response.list[i].weather[0].description);    
-                var condition=$("<p>");
-                    condition.text(" The Cloud is: " + response.list[i].clouds.all + "%");
+                var condition=$("<p>").text(" The Cloud is: " + response.list[i].clouds.all + "%");
                     console.log(" The Cloud is: " + response.list[i].clouds.all + "%");
-                var wind=$("<p>");
-                    wind.text(" The Wind is: " + response.list[i].wind.speed);
+                var wind=$("<p>").text(" The Wind is: " + response.list[i].wind.speed);
                     console.log(" The Wind is: " + response.list[i].wind.speed);
                 weatherInfo.append(date);
                 weatherInfo.append(weather);
                 weatherInfo.append(condition);
                 weatherInfo.append(wind);
-                
-           
             }
         
-               $("#weatherDisplay").html(weatherInfo);  
-      
-            });
+            $("#weatherDisplay").html(weatherInfo);  
+            //     $("#userSearch").append(".link");
+            // });
 });
 
-    $("#mapsBtn").on("click", function(){
-        $("#weatherDisplay").hide();
+    // $("#mapsBtn").on("click", function(){
+    //     $("#weatherDisplay").hide();
+
+    // });
+    // $("#satBtn").on("click", function(){
+    //     $("#weatherDisplay").hide();
+
+    // });
+    // $("#weathBtn").on("click", function(){
+    //     $("#weatherDisplay").show();
+
+    // });
+
+    // $("#likeBtn").on("click", function(){
 
     });
-    $("#satBtn").on("click", function(){
-        $("#weatherDisplay").hide();
-
-    });
-    $("#weathBtn").on("click", function(){
-        $("#weatherDisplay").show();
-
-    });
-
 
     });
